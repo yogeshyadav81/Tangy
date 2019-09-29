756f5f3011b7bb93653f9d09d1e90662af9fe21c
yogeshyadav81 <yogesh.yadav81@gmail.com>
2019-09-29 15:26:14 +0530

    for change VM size

---
+++ updateVMProperty.ps1
@@ -0,0 +1,15 @@
+﻿param (
+    [string]$resourceGroupName,
+    [string]$vmName,    
+    [string]$vmSize
+    
+)
+
+   #Stop-AzureRmVM -ResourceGroupName $resourceGroupName -Name $vmName -Force
+   
+   $vm = Get-AzureRmVM -ResourceGroupName $resourceGroupName -Name $vmName
+   $vm.HardwareProfile.VmSize = 'Standard_D2_v2' #$vmSize
+
+   Update-AzureRmVM -ResourceGroupName $resourceGroupName -VM $vm
+
+   #Start-AzureRmVM -ResourceGroupName $resourceGroupName -Name $vm.Name
